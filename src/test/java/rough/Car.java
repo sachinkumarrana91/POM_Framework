4390638a7aa29eb15198406617eb3455691aab35
Sachin Kumar <sachin.kumar@bold.com>
2020-03-03 13:55:09 +0530

    test webhook

--- src/test/java/rough/Car.java
+++ src/test/java/rough/Car.java
@@ -2,6 +2,12 @@ package rough;
 
 public class Car {
 
+
+//System.out.println("check WebHooks only");
+
+
+//System.out.println("check WebHooks only");
+
 	String carModel;
 	Registration_no reg;
 
